abffc6c728c5c989759eb2aa64d522c56ea3eab2
Oksana Senina <o.senina@adguard.com>
2025-10-31 12:23:06 +0300

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,22 @@
+{\rtf1\ansi\ansicpg1251\cocoartf2865
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs26 \cf0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 // ==UserScript==\
+// @name         script doing nothing\
+// @version      0.3\
+// @description  what a shame\
+// @match        *://*/*\
+// @grant        unsafeWindow\
+// @run-at       document-start\
+// ==/UserScript==\
+\
+(function() \{\
+\
+\})();\
+}
