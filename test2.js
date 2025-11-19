effe8ed482f884b773a07c22219cc1706677c366
Oksana Senina <o.senina@adguard.com>
2025-11-19 15:04:43 +0300

    test

--- test2.js
+++ test2.js
@@ -1,12 +1,2 @@
-// ==UserScript==
-// @name         script doing nothing
-// @version      0.2
-// @description  what a shame
-// @match        *://*/*
-// @grant        unsafeWindow
-// @run-at       document-start
-// ==/UserScript==
-
-(function() {
-
-})();
+lenta.ru
+meduza.io
